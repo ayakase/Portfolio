ab061c583887643aface29256aa33de9cffaab26
ayakase <wibu1892001@gmail.com>
2024-03-05 13:03:31 +0700

    backup

--- src/App.svelte
+++ src/App.svelte
@@ -82,17 +82,7 @@
     {/each}
   </div>
   <div id="test"></div>
-  <!-- component -->
-  <!--
-  Welcome to Tailwind Play, the official Tailwind CSS playground!
 
-  Everything here works just like it does when you're running Tailwind locally
-  with a real build pipeline. You can customize your config file, use features
-  like `@apply`, or even add third-party plugins.
-
-  Feel free to play with this example if you're just learning, or trash it and
-  start from scratch if you know enough to be dangerous. Have fun!
--->
 </div>
 
 <style>
