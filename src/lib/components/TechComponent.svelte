1c6454cba48e0fdd998c74c5cdc73db0fbb10f5e
ayakase <wibu1892001@gmail.com>
2024-03-10 01:32:55 +0700

    change icon

--- src/lib/components/TechComponent.svelte
+++ src/lib/components/TechComponent.svelte
@@ -1,173 +1,154 @@
 <div
-    class="m-auto p-auto w-[90%] relative isolate overflow-hidden py-24 sm:py-32 lg:overflow-visible lg:px-0"
+  class="m-auto p-auto w-[90%] relative isolate overflow-hidden py-24 sm:py-32 lg:overflow-visible lg:px-0"
 >
+  <div
+    class=" grid grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10"
+  >
     <div
-        class=" grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10"
+      class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:grid w-full lg:grid-cols-2 lg:gap-x-8"
     >
-        <div
-            class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8"
-        >
-            <div class="lg:pr-4">
-                <div class="">
-                    <p class="text-4xl font-semibold leading-7 text-white">
-                        What do I do ?
-                    </p>
-                    <!-- <h1
+      <div class="">
+        <div class="">
+          <p class="text-4xl font-semibold leading-7 text-white">
+            What do I do ?
+          </p>
+          <!-- <h1
                         class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
                     >
                         I like to code
                     </h1> -->
-                    <p class="mt-6 text-xl leading-8 text-gray-100">
-                        I started my University with
-                    </p>
-                </div>
-            </div>
+          <p class="mt-6 text-xl leading-8 text-gray-100">
+            I started my University with
+          </p>
         </div>
-        <div
-            class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden"
-        >
-            <img
-                class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-                alt=""
-            />
-        </div>
-        <div
-            class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8"
-        >
-            <div class="lg:pr-4">
-                <div
-                    class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg"
-                >
-                    <p>
-                        Faucibus commodo massa rhoncus, volutpat. Dignissim sed
-                        eget risus enim. Mattis mauris semper sed amet vitae sed
-                        turpis id. Id dolor praesent donec est. Odio penatibus
-                        risus viverra tellus varius sit neque erat velit.
-                        Faucibus commodo massa rhoncus, volutpat. Dignissim sed
-                        eget risus enim. Mattis mauris semper sed amet vitae sed
-                        turpis id.
-                    </p>
-                    <ul role="list" class="mt-8 space-y-8 text-gray-600">
-                        <li class="flex gap-x-3">
-                            <svg
-                                class="mt-1 h-5 w-5 flex-none text-green-400"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                            <span class="text-gray-100"
-                                ><strong class="font-semibold"
-                                    >Front-end:</strong
-                                > Vue 3, Nuxt 3, Pinia, Vuetify, Svelte, ThreeJS,
-                                Bootstrap, BulmaCSS, TailwindCSS, AceternityUI</span
-                            >
-                        </li>
-                        <li class="flex gap-x-3">
-                            <svg
-                                class="mt-1 h-5 w-5 flex-none text-green-400"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                            <span class="text-gray-100"
-                                ><strong class="font-semibold">Back-end:</strong
-                                > NodeJS, ExpressJS, Flask, Supabase, Firebase, Ruby
-                                on Rails, Socket.IO</span
-                            >
-                        </li>
-                        <li class="flex gap-x-3">
-                            <svg
-                                class="mt-1 h-5 w-5 flex-none text-green-400"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                            <span class="text-gray-100"
-                                ><strong class="font-semibold"
-                                    >Database:
-                                </strong> MySQL, MongoDB, Firestore 10</span
-                            >
-                        </li>
-                        <li class="flex gap-x-3">
-                            <svg
-                                class="mt-1 h-5 w-5 flex-none text-green-400"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                            <span class="text-gray-100"
-                                ><strong class="font-semibold">Cloud: </strong> AWS
-                                EC2, Cloudinary</span
-                            >
-                        </li>
-                        <li class="flex gap-x-3">
-                            <svg
-                                class="mt-1 h-5 w-5 flex-none text-green-400"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                            <span class="text-gray-100"
-                                ><strong class="font-semibold">Other </strong> Git,
-                                Github, Linux, Bash, Vite
-                            </span>
-                        </li>
-                    </ul>
-                    <p class="mt-8">
-                        Et vitae blandit facilisi magna lacus commodo. Vitae
-                        sapien duis odio id et. Id blandit molestie auctor
-                        fermentum dignissim. Lacus diam tincidunt ac cursus in
-                        vel. Mauris varius vulputate et ultrices hac adipiscing
-                        egestas. Iaculis convallis ac tempor et ut. Ac lorem vel
-                        integer orci.
-                    </p>
-                    <h2
-                        class="mt-16 text-2xl font-bold tracking-tight text-gray-900"
-                    >
-                        No server? No problem.
-                    </h2>
-                    <p class="mt-6">
-                        Id orci tellus laoreet id ac. Dolor, aenean leo, ac
-                        etiam consequat in. Convallis arcu ipsum urna nibh.
-                        Pharetra, euismod vitae interdum mauris enim, consequat
-                        vulputate nibh. Maecenas pellentesque id sed tellus
-                        mauris, ultrices mauris. Tincidunt enim cursus ridiculus
-                        mi. Pellentesque nam sed nullam sed diam turpis ipsum eu
-                        a sed convallis diam.
-                    </p>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
+    <div
+      class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden"
+    >
+      <img
+        class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+        src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+        alt=""
+      />
+    </div>
+    <div
+      class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8"
+    >
+      <div class="">
+        <div class=" text-base leading-7 text-gray-700">
+          <p>
+            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
+            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
+            praesent donec est. Odio penatibus risus viverra tellus varius sit
+            neque erat velit. Faucibus commodo massa rhoncus, volutpat.
+            Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
+            sed turpis id.
+          </p>
+          <ul role="list" class="mt-8 space-y-8 text-gray-600">
+            <li class="flex gap-x-3">
+              <svg
+                class="mt-1 h-5 w-5 flex-none text-green-400"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
+                  ><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+                    d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64H240l-10.7 32H160c-17.7 0-32 14.3-32 32s14.3 32 32 32H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H346.7L336 416H512c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64zM512 64V352H64V64H512z"
+                  /></svg
+                >
+              </svg>
+              <span class="text-gray-100"
+                ><strong class="font-semibold">Front-end: </strong> Vue 3, Nuxt 3,
+                Pinia, Vuetify, Svelte, ThreeJS, Bootstrap, BulmaCSS, TailwindCSS,
+                AceternityUI</span
+              >
+            </li>
+            <li class="flex gap-x-3">
+              <svg
+                class="mt-1 h-5 w-5 flex-none text-green-400"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 32C28.7 32 0 60.7 0 96v64c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm280 72a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm48 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0zM64 288c-35.3 0-64 28.7-64 64v64c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V352c0-35.3-28.7-64-64-64H64zm280 72a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm56 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg>
+              </svg>
+              <span class="text-gray-100"
+                ><strong class="font-semibold">Back-end: </strong> NodeJS, ExpressJS,
+                Flask, Supabase, Firebase, Ruby on Rails, Socket.IO</span
+              >
+            </li>
+            <li class="flex gap-x-3">
+              <svg
+                class="mt-1 h-5 w-5 flex-none text-green-400"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+                  ><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+                    d="M448 80v48c0 44.2-100.3 80-224 80S0 172.2 0 128V80C0 35.8 100.3 0 224 0S448 35.8 448 80zM393.2 214.7c20.8-7.4 39.9-16.9 54.8-28.6V288c0 44.2-100.3 80-224 80S0 332.2 0 288V186.1c14.9 11.8 34 21.2 54.8 28.6C99.7 230.7 159.5 240 224 240s124.3-9.3 169.2-25.3zM0 346.1c14.9 11.8 34 21.2 54.8 28.6C99.7 390.7 159.5 400 224 400s124.3-9.3 169.2-25.3c20.8-7.4 39.9-16.9 54.8-28.6V432c0 44.2-100.3 80-224 80S0 476.2 0 432V346.1z"
+                  /></svg
+                >
+              </svg>
+              <span class="text-gray-100"
+                ><strong class="font-semibold">Database: </strong> MySQL, MongoDB,
+                Firestore 10</span
+              >
+            </li>
+            <li class="flex gap-x-3">
+              <svg
+                class="mt-1 h-5 w-5 flex-none text-green-400"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"
+                  ><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+                    d="M0 336c0 79.5 64.5 144 144 144H512c70.7 0 128-57.3 128-128c0-61.9-44-113.6-102.4-125.4c4.1-10.7 6.4-22.4 6.4-34.6c0-53-43-96-96-96c-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32C167.6 32 96 103.6 96 192c0 2.7 .1 5.4 .2 8.1C40.2 219.8 0 273.2 0 336z"
+                  /></svg
+                >
+              </svg>
+              <span class="text-gray-100"
+                ><strong class="font-semibold">Cloud: </strong> AWS EC2, Cloudinary</span
+              >
+            </li>
+            <li class="flex gap-x-3">
+              <svg
+                class="mt-1 h-5 w-5 flex-none text-green-400"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M176 88v40H336V88c0-4.4-3.6-8-8-8H184c-4.4 0-8 3.6-8 8zm-48 40V88c0-30.9 25.1-56 56-56H328c30.9 0 56 25.1 56 56v40h28.1c12.7 0 24.9 5.1 33.9 14.1l51.9 51.9c9 9 14.1 21.2 14.1 33.9V304H384V288c0-17.7-14.3-32-32-32s-32 14.3-32 32v16H192V288c0-17.7-14.3-32-32-32s-32 14.3-32 32v16H0V227.9c0-12.7 5.1-24.9 14.1-33.9l51.9-51.9c9-9 21.2-14.1 33.9-14.1H128zM0 416V336H128v16c0 17.7 14.3 32 32 32s32-14.3 32-32V336H320v16c0 17.7 14.3 32 32 32s32-14.3 32-32V336H512v80c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64z"/></svg>
+              </svg>
+              <span class="text-gray-100"
+                ><strong class="font-semibold">Other tools: </strong> Git, Github, Linux,
+                Bash, Vite
+              </span>
+            </li>
+          </ul>
+          <p class="mt-8">
+            Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
+            odio id et. Id blandit molestie auctor fermentum dignissim. Lacus
+            diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices
+            hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem
+            vel integer orci.
+          </p>
+          <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">
+            No server? No problem.
+          </h2>
+          <p class="mt-6">
+            Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat
+            in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum
+            mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
+            tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
+            Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
+            diam.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
