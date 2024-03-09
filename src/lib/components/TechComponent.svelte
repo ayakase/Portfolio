c3b078c6f0e4aab2ba9b642c8516845b480f40a7
ayakase <wibu1892001@gmail.com>
2024-03-09 22:04:56 +0700

    dep

--- src/lib/components/TechComponent.svelte
+++ src/lib/components/TechComponent.svelte
@@ -65,8 +65,8 @@
                             <span class="text-gray-100"
                                 ><strong class="font-semibold"
                                     >Front-end:</strong
-                                > Vue 3, Nuxt 3, Vuetify, Svelte, ThreeJS, Bootstrap,
-                                BulmaCSS, TailwindCSS, AceternityUI</span
+                                > Vue 3, Nuxt 3, Pinia, Vuetify, Svelte, ThreeJS,
+                                Bootstrap, BulmaCSS, TailwindCSS, AceternityUI</span
                             >
                         </li>
                         <li class="flex gap-x-3">
@@ -84,8 +84,8 @@
                             </svg>
                             <span class="text-gray-100"
                                 ><strong class="font-semibold">Back-end:</strong
-                                > NodeJS, Flask, Supabase, Firebase, Ruby on Rails,
-                                Socket.IO</span
+                                > NodeJS, ExpressJS, Flask, Supabase, Firebase, Ruby
+                                on Rails, Socket.IO</span
                             >
                         </li>
                         <li class="flex gap-x-3">
@@ -124,6 +124,24 @@
                                 ><strong class="font-semibold">Cloud: </strong> AWS
                                 EC2, Cloudinary</span
                             >
+                        </li>
+                        <li class="flex gap-x-3">
+                            <svg
+                                class="mt-1 h-5 w-5 flex-none text-green-400"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                            <span class="text-gray-100"
+                                ><strong class="font-semibold">Other </strong> Git,
+                                Github, Linux, Bash, Vite
+                            </span>
                         </li>
                     </ul>
                     <p class="mt-8">
