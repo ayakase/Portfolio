45deea13a6000b16c77e66515543bb3a814be94c
ayakase <wibu1892001@gmail.com>
2024-03-10 16:35:06 +0700

    fix mobile nav

--- src/lib/components/MobileNav.svelte
+++ src/lib/components/MobileNav.svelte
@@ -1,25 +1,90 @@
-<div id="circularMenu" class="circular-menu active z-50">
+<div id="circularMenu" class="circular-menu z-50 active sm:block lg:hidden">
   <!-- svelte-ignore a11y-missing-attribute -->
   <a
     class="floating-btn"
     onclick="document.getElementById('circularMenu').classList.toggle('active');"
   >
-    <i class="fa fa-plus"></i>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      width="2.4rem"
+      fill="white"
+      class="menu-icon text-white"
+      ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+        ><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+          d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
+        /></svg
+      ></svg
+    >
   </a>
 
   <menu class="items-wrapper">
-    <a href="#" class="menu-item"></a>
-    <a href="#technical" class="menu-item"></a>
-    <a href="#experience" class="menu-item"></a>
-    <a href="#projects" class="menu-item"></a>
+    <a href="#" class="menu-item">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        width="1.2rem"
+        fill="white"
+        class="menu-icon text-white"
+        ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+          ><path
+            d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"
+          /></svg
+        ></svg
+      >
+    </a>
+    <a href="#technical" class="menu-item">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        width="1.2rem"
+        fill="white"
+        class="menu-icon text-white"
+        ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+          ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+            ><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+              d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"
+            /></svg
+          ></svg
+        ></svg
+      >
+    </a>
+    <a href="#experience" class="menu-item">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        width="1.2rem"
+        fill="white"
+        class="menu-icon text-white"
+        ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
+          ><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+            d="M64 48c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16h80V400c0-26.5 21.5-48 48-48s48 21.5 48 48v64h80c8.8 0 16-7.2 16-16V64c0-8.8-7.2-16-16-16H64zM0 64C0 28.7 28.7 0 64 0H320c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm88 40c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16v48c0 8.8-7.2 16-16 16H104c-8.8 0-16-7.2-16-16V104zM232 88h48c8.8 0 16 7.2 16 16v48c0 8.8-7.2 16-16 16H232c-8.8 0-16-7.2-16-16V104c0-8.8 7.2-16 16-16zM88 232c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16v48c0 8.8-7.2 16-16 16H104c-8.8 0-16-7.2-16-16V232zm144-16h48c8.8 0 16 7.2 16 16v48c0 8.8-7.2 16-16 16H232c-8.8 0-16-7.2-16-16V232c0-8.8 7.2-16 16-16z"
+          /></svg
+        ></svg
+      >
+    </a>
+    <a href="#projects" class="menu-item">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        width="1.2rem"
+        fill="white"
+        class="menu-icon text-white"
+        ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+          ><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+            d="M50.7 58.5L0 160H208V32H93.7C75.5 32 58.9 42.3 50.7 58.5zM240 160H448L397.3 58.5C389.1 42.3 372.5 32 354.3 32H240V160zm208 32H0V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V192z"
+          /></svg
+        ></svg
+      >
+    </a>
     <a href="#contact" class="menu-item">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 512 512"
-        width="1rem"
+        width="1.2rem"
         fill="white"
         class="menu-icon text-white"
-        ><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+        ><path
           d="M280 0C408.1 0 512 103.9 512 232c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-101.6-82.4-184-184-184c-13.3 0-24-10.7-24-24s10.7-24 24-24zm8 192a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm-32-72c0-13.3 10.7-24 24-24c75.1 0 136 60.9 136 136c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-48.6-39.4-88-88-88c-13.3 0-24-10.7-24-24zM117.5 1.4c19.4-5.3 39.7 4.6 47.4 23.2l40 96c6.8 16.3 2.1 35.2-11.6 46.3L144 207.3c33.3 70.4 90.3 127.4 160.7 160.7L345 318.7c11.2-13.7 30-18.4 46.3-11.6l96 40c18.6 7.7 28.5 28 23.2 47.4l-24 88C481.8 499.9 466 512 448 512C200.6 512 0 311.4 0 64C0 46 12.1 30.2 29.5 25.4l88-24z"
         /></svg
       >
@@ -35,12 +100,15 @@
   }
 
   .circular-menu .floating-btn {
-    display: block;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-start;
+    padding: 0.4rem;
     width: 5em;
     height: 5em;
     border-bottom-left-radius: 999px;
     backdrop-filter: blur(4px);
-    background-color: hsl(4, 98%, 60%);
+    background-color: #3f51b5;
     box-shadow: 0 2px 5px 0 hsla(0, 0%, 0%, 0.26);
     color: hsl(0, 0%, 100%);
     text-align: center;
@@ -57,15 +125,6 @@
     box-shadow: 0 4px 8px 0 hsla(0, 0%, 0%, 0.4);
   }
 
-  .circular-menu .floating-btn i {
-    font-size: 1.3em;
-    transition: transform 0.2s;
-  }
-
-  .circular-menu.active .floating-btn i {
-    transform: rotate(-45deg);
-  }
-
   .circular-menu:after {
     display: block;
     content: " ";
@@ -76,8 +135,8 @@
     top: 0;
     right: 0;
     z-index: -2;
-    background: rgba(175, 76, 170, 0.3);
-    backdrop-filter: blur(4px);
+    background: rgba(66, 58, 183, 0.37);
+    backdrop-filter: blur(2px);
     transition: all 0.3s ease;
   }
 
@@ -105,7 +164,7 @@
     border-radius: 50%;
     text-align: center;
     line-height: 3;
-    background-color: hsla(0, 0%, 0%, 0.1);
+
     transition:
       transform 0.3s ease,
       background 0.2s ease;
@@ -113,7 +172,7 @@
   .menu-item {
     display: flex !important;
     flex-direction: row;
-    background-color: rebeccapurple !important;
+    background: rgba(58, 71, 183, 0.37);
     justify-content: center;
     align-items: center;
   }
