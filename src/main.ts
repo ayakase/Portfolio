7cd60f09e5870445e168c37412868d1961bc55b5
ayakase <wibu1892001@gmail.com>
2024-03-14 07:06:28 +0700

    fix mobile load 3d scene

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import './app.css'
 import App from './App.svelte'
 import { initializeScene } from './lib/utils/threeScene'
-initializeScene()
+function runOnSmallScreen() {
+  if (window.innerWidth > 768) { 
+    initializeScene()
+    console.log('render 3d scene')
+  }
+}
+runOnSmallScreen()
 const app = new App({
   target: document.getElementById('app')!,
 })
