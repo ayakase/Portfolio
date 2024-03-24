a33494a2ac58712d3f575cd0eee5603fbc2e5789
ayakase <wibu1892001@gmail.com>
2024-03-24 15:56:57 +0700

    final

--- src/lib/components/ui/TextGenerateEffect/TextGenerateEffect.svelte
+++ src/lib/components/ui/TextGenerateEffect/TextGenerateEffect.svelte
@@ -1,46 +1,46 @@
 <script lang="ts">
-    import { Motion } from "svelte-motion";
-    import { cn } from "../../../utils/cn";
-    export let words: any;
-    export let className = void 0;
-    const variants = {
-        visible: (i: any) => ({
-            opacity: 1,
-            transition: {
-                delay: i * 0.1,
-                duration: 1,
-            },
-        }),
-        hidden: { opacity: 0 },
-    };
+  import { Motion } from "svelte-motion";
+  import { cn } from "../../../utils/cn";
+  export let words: any;
+  export let className = void 0;
+  const variants = {
+    visible: (i: any) => ({
+      opacity: 1,
+      transition: {
+        delay: i * 0.2,
+        duration: 1,
+      },
+    }),
+    hidden: { opacity: 0 },
+  };
 </script>
 
 <div class={cn("font-bold", className)}>
-    <div class="">
-        <div class=" text-6xl leading-snug tracking-wide text-white">
+  <div class="">
+    <div class=" text-6xl leading-snug tracking-wide text-white">
+      <Motion
+        let:motion
+        custom={0}
+        {variants}
+        initial="hidden"
+        animate={"visible"}
+      >
+        <div use:motion>
+          {#each words.split(" ") as word, idx (`${word}${idx}`)}
             <Motion
-                let:motion
-                custom={0}
-                {variants}
-                initial="hidden"
-                animate={"visible"}
+              let:motion
+              {variants}
+              custom={idx + 1}
+              initial="hidden"
+              animate={"visible"}
             >
-                <div use:motion>
-                    {#each words.split(" ") as word, idx (`${word}${idx}`)}
-                        <Motion
-                            let:motion
-                            {variants}
-                            custom={idx + 1}
-                            initial="hidden"
-                            animate={"visible"}
-                        >
-                            <span use:motion class="text-white">
-                                {word}{" "}
-                            </span>
-                        </Motion>
-                    {/each}
-                </div>
+              <span use:motion class="text-white">
+                {word}{" "}
+              </span>
             </Motion>
+          {/each}
         </div>
+      </Motion>
     </div>
+  </div>
 </div>
